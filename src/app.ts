8c6a6c39f25f5adcdfbe8e08436b63152d4168b0
Rupert Smith <rupertwebdev@gmail.com>
2023-10-16 16:29:33 +0100

    Files cleaned up and pre commitsetup added

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 /* eslint-disable */
-import express, { Express, json } from 'express';
-import 'express-async-errors';
-import cors from 'cors';
-import { Logger, handleError, type CommonRoutesConfig } from '@ipcortex/commons';
-import { ServiceRoutes } from './routes/ServiceRoutes';
+import express, { Express, json } from "express";
+import "express-async-errors";
+import cors from "cors";
+import {
+  Logger,
+  handleError,
+  type CommonRoutesConfig,
+} from "@ipcortex/commons";
+import { ServiceRoutes } from "./routes/ServiceRoutes";
 
 const app: Express = express();
 
-const logger = Logger('routing-service-v2:app.ts');
+const logger = Logger("service-template-v2:app.ts");
 
 const routes: CommonRoutesConfig[] = [];
 
@@ -15,17 +19,16 @@ app.use(json());
 app.use(cors());
 
 try {
-    app.get('/', (req, res) => {
-        res.status(200).send('Endpoint is alive!');
-    });
+  app.get("/", (req, res) => {
+    res.status(200).send("Service template is alive!");
+  });
 
-    routes.push(new ServiceRoutes(app));
-
+  routes.push(new ServiceRoutes(app));
 } catch (err) {
-    logger.error({
-        message: 'Server error',
-        error: err
-    });
+  logger.error({
+    message: "Server error",
+    error: err,
+  });
 }
 
 app.use(handleError);
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import { app } from './app';
-import { Logger } from '@ipcortex/commons';
-import { config } from './config/ServiceConfig';
+import { app } from "./app";
+import { Logger } from "@ipcortex/commons";
+import { config } from "./config/ServiceConfig";
 
-const logger = Logger('routing-service-v2:server.ts');
+const logger = Logger("service-template-v2:server.ts");
 const port = config.PORT;
 
 try {
-    app.listen(port, () => {
-        logger.info(`Server is running at http://localhost:${port}`);
-    });
+  app.listen(port, () => {
+    logger.info(`Server is running at http://localhost:${port}`);
+  });
 } catch (err) {
-    logger.error({
-        message: 'Could not start server',
-        error: err
-    });
+  logger.error({
+    message: "Could not start server",
+    error: err,
+  });
 }
