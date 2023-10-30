9038098f4866285bc860b3af861cf8ce7c13f55d
Rupert Smith <rupertwebdev@gmail.com>
2023-10-30 11:56:35 +0000

    Instructions remvoed

--- src/routes/ServiceTemplateRoutes.ts
+++ src/routes/ServiceTemplateRoutes.ts
@@ -7,20 +7,6 @@ export class ServiceTemplateRoutes extends CommonRoutesConfig {
     super(app, 'ServiceTemplateRoutes');
   }
 
-  // 1. get helloworld. Returns 'helloworld' as a string
-  // 2. post helloworld. Expects a name...saves this name to the database with an id.
-  // responds with the appropriate status code for 'created'
-  // 3. get helloworld/id. Returns 'helloworld' + name associated with this id
-  // 4. post helloworld/id. Updates the name associated with the given id in the database.
-
-  // In order to carry out the above requests, you will need to edit the DAOs, model and controller.
-  // You will need to delete and redo the UpsertDTO
-  // You will also need to create a prismaschema for the database
-
-  // update the docs to be in line with these end points you are creating
-
-  // make each request a pull request. Take one by one
-
   configureRoutes(): Express {
     const serviceTemplateController = new ServiceTemplateController();
     // below are some examples of route handlers
